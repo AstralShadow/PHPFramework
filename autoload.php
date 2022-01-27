22e594020c20537a1b4e554fe3048ea55481641f
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-01-27 09:32:14 +0200

    [beautify] Removed recursion from autoloader script

--- autoload.php
+++ autoload.php
@@ -8,31 +8,44 @@
 
 require "Core/utils.php";
 
-spl_autoload_register(function (string $class): void{
+spl_autoload_register(function (string $class) : void
+{
     $file = str_replace('\\', '/', $class . '.php');
-    if (file_exists($file)){
+    if(file_exists($file))
+    {
         require $file;
         \Core\initClass($class);
         return;
     }
 
-    $caseInsensitiveLoader ??= function (array $path, $filename = '.')
-    use (&$caseInsensitiveLoader){
-        if (count($path) && is_dir($filename)){
-            foreach (scandir($filename) as $item){
-                if (strtolower($item) === strtolower($path[0])){
-                    $newPath = array_slice($path, 1);
-                    $newFilename = $filename . '/' . $item;
-                    return $caseInsensitiveLoader($newPath, $newFilename);
-                }
-            }
-        } else if (file_exists($filename)){
-            require $filename;
-            return;
-        }
-    };
 
-    $path = array_map('strtolower', explode('/', $file));
-    $caseInsensitiveLoader($path);
-    \Core\initClass($class);
+    // The following code is case insensitive path search,
+    //  in case you prefer different file naming conventions.
+    // It actually should not affect some systems, so you
+    //  better not remove it unless you know what you're doing.
+    // Still, i recommend that you don't happen to use it,
+    //  because scanning directories is slower than knowing
+    //  the correct path from the beginning.
+
+    $path = explode('/', strtolower($file));
+    $target = '.';
+
+    while(count($path) && is_dir($target))
+    {
+        foreach(scandir($target) as $item)
+        {
+            if(strtolower($item) == $path[0])
+            {
+                $path = array_slice($path, 1);
+                $target = $target . '/' . $item;
+            }
+        }
+    }
+
+    if(file_exists($target))
+    {
+        require $target;
+        \Core\initClass($class);
+    }
 });
+
